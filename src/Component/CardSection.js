ea7ceaa3971d0bb601737756974724895062697e
webdevsejin <webdevsejin@gmail.com>
2023-01-12 16:08:06 +0900

    feat: README.md update

--- src/Component/CardSection.js
+++ src/Component/CardSection.js
@@ -8,10 +8,6 @@ export default function Section1() {
    <SectionCard/>
    <SectionCard1/>
    <SectionCard2/>
-
-   </>
-   
-  
+   </> 
   );
-
 }
--- src/Component/MainAlert.js
+++ src/Component/MainAlert.js
@@ -3,9 +3,16 @@ import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
 
 export default function MainAlert() {
+  setTimeout(function () {
+    alert(<MainAlert/>);
+  }, 3000);
+
   return (
+    
     <Stack sx={{ width: '100%' }} spacing={2}>
+    <div className='MainAlert'>
       <Alert severity="info">최신 소식과 할인정보를 만나보세요!</Alert>
+      </div>
     </Stack>
   );
 }
