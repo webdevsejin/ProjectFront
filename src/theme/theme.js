df22cdac22eb79441ac4268a4bb2301039ebaea8
webdevsejin <webdevsejin@gmail.com>
2023-01-18 16:59:03 +0900

    feat: README.md update

---
+++ src/theme/theme.js
@@ -0,0 +1,16 @@
+import { borderColor } from "@mui/system";
+
+export const LightTheme = {
+    bgColor: 'F8F7F4',
+    textColor: '#31302E',
+    borderColor:'1px solid #eaeaea'
+};
+export const darkTheme = {
+    bgColor: '#1E1E22',
+    textColor:'#ccc' , 
+    borderColor: '1px solid #2c2d33'
+};
+export const theme = {
+    LightTheme,
+    darkTheme,
+};
